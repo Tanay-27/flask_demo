8d519478cc35376e5e8416f42a366c15bc2d36a3
Tanay Shah <tanayshah027@gmail.com>
2020-07-09 10:58:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+#import numpy as np
+from flask import Flask, request, render_template, url_for
+from instamojo_wrapper import Instamojo
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
+API_KEY = 'test_a7fe6937b28c505f371ce8ca286'
+AUTH_TOKEN = 'test_a68ddfad2ea46762db39aadd1e3'
+api = Instamojo(api_key=API_KEY,
+                auth_token=AUTH_TOKEN,
+                endpoint='https://test.instamojo.com/api/1.1/')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+response = api.payment_request_create(
+	amount=25,
+	purpose="app test",
+	send_email=True,
+	email="tanayshah027@gmail.com",
+	redirect_url="https://amvmpayments-api.herokuapp.com/payments",
+	)
 
-@app.route('/predict',methods=['POST'])
-def predict():
+#@app.route('/')
+#def home():
+#    amt = 15
+#    pur = "Check"
+#    #x = createPayment(amt,pur)
+#    return render_template('index.html')
+
+@app.route('/payments',methods=['POST'])
+def paymentredirect():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    id = request.args.get('payment_id')
+    stat = request.args.get('payment_status')
+    req = request.args.get('payment_request_id')
+    print(request.form['payment_request'])  # should display 'bar'
+    dict = {
+        'text':"Thankyou for purchasing",
+        'id': id,
+        'status':stat,
+        'req':req
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
+    }
+    return render_template('index.html',dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
